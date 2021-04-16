a546d55ca72dbebebc456507f9b5a2156d9fa1df
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-04-16 09:09:17 +0200

    console logs removed

--- components/BlogpostList.js
+++ components/BlogpostList.js
@@ -1,7 +1,5 @@
 import BlogpostItem from "./BlogpostItem";
 const BlogpostList = ({ blogposts }) => {
-  console.log("blogposts");
-  console.log(blogposts);
   return (
     <>
       <div className="row mb-2">
